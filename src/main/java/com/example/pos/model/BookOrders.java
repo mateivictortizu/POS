06c28755ad0c26c906c797fb5114a8d38f623f80
Matei <matteovk@icloud.com>
2021-12-06 00:11:21 +0200

    MongoDB

--- src/main/java/com/example/pos/model/BookOrders.java
+++ src/main/java/com/example/pos/model/BookOrders.java
@@ -1,16 +1,19 @@
 package com.example.pos.model;
 
+import net.minidev.json.JSONObject;
+import netscape.javascript.JSObject;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Map;
+import java.util.List;
 
 @Document(collection = "#{@configRepositoryCustom.getCollectionName()}")
 public class BookOrders implements Serializable {
     private Date date;
     private String status;
+
+    private List<JSONObject> items;
 
     public String getStatus() {
         return status;
@@ -26,6 +29,14 @@ public class BookOrders implements Serializable {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public void setItems(List<JSONObject> items) {
+        this.items = items;
+    }
+
+    public List<JSONObject> getItems() {
+        return items;
     }
 
     @Override
