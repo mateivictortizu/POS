664333b6e4100d175340c4d842d2087d0601a690
Matei <matteovk@icloud.com>
2021-12-06 00:27:20 +0200

    MongoDB completed

--- src/main/java/com/example/pos/model/BookOrders.java
+++ src/main/java/com/example/pos/model/BookOrders.java
@@ -1,19 +1,63 @@
 package com.example.pos.model;
 
 import net.minidev.json.JSONObject;
-import netscape.javascript.JSObject;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+
+@Embeddable
+class Order{
+    String ISBN = null;
+    String title = null;
+    Integer price = null;
+    Integer quantity = null;
+
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
 
 @Document(collection = "#{@configRepositoryCustom.getCollectionName()}")
 public class BookOrders implements Serializable {
     private Date date;
     private String status;
 
-    private List<JSONObject> items;
+    @Embedded
+    private List<Order> items;
 
     public String getStatus() {
         return status;
@@ -31,11 +75,11 @@ public class BookOrders implements Serializable {
         this.date = date;
     }
 
-    public void setItems(List<JSONObject> items) {
+    public void setItems(List<Order> items) {
         this.items = items;
     }
 
-    public List<JSONObject> getItems() {
+    public List<Order> getItems() {
         return items;
     }
 
